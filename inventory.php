f6c7248586a6a8f3a86c9f398502fa319a9fe64a
Breeana Gant <breeanagant@Breeanas-MacBook-Pro.local>
2022-10-25 15:56:24 -0400

    Added inventory.php file

--- inventory.php
+++ inventory.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <link href="aims.css" rel="stylesheet">
-    <title>AIMS::Homepage</title>
+    <title>AIMS::Inventory</title>
 </head>
 
 <body>
@@ -20,7 +20,7 @@
             <li><a href="index.html">Home</a></li>
             <li><a href="about.html">About</a></li>
             <li><a href="rental.html">Rental Information</a></li>
-            <li><a href="inventory.php">Inventory</a></li>
+            <li><a href="inventory.html">Inventory</a></li>
             <li><a href="contact.html">Contact Us</a></li>
             <li><a href="signon.html">Sign In</a></li>
         </ul>
@@ -29,16 +29,25 @@
     <!-- Main -->
     <div id="content">
     <main>
-        <h2> Welcome to the Activities Inventory Management System</h2>
-      <p><h3><b>Rental Application Instructions</b></h3></p>
-      <ol>
-        <li>Visit the <a href="inventory.html">Inventory</a>page to view all currently available inventory.</li>
-        <li>Visit the <a href="rental.html">Rental Application</a> page to complete the form.</li>
-        <li>Fill out the form fields.</li>
-        <li>Use the Google Calendar field to schedule your pickup and dropoff times.</li>
-      </ol>
+        <h2> Current Inventory</h2>
+        <? php
+            $username = "admin";
+            $password = "Tiff1tre2maddy3";
+            $host = "aims-demo1.cjiww4oiz41u.us-east-1.rds.amazonaws.com";
+            $database = "aims";
 
-      <p>If you are having issues accessing the form, <a href="contact.html">Please contact and Administrator or our Technical Support Staff</a></p>
+            $mysqli = new mysqli($host, $username, $password, $database);
+            $query = "SELECT * FROM inventory";
+            if ($result = $mysqli->query($query)) {
+
+                while ($row = $result->fetch_assoc()) {
+                    $field1name = $row["IID"];
+                    $field2name = $row["IIEM_NAME"];
+
+
+                    echo '<b>'.$field1name.$field2name.'</b><br />';
+    }
+        ?>
     </main>
     </div>
     <!-- Footer -->
